23301b7a7d57589563c74a55eb752d8c8004ee93
Marcin Przybyłowicz <martin.przybylowicz@gmail.com>
2021-04-23 09:52:18 +0200

    Refactoring.

--- SvgDesigner/SvgDesigner/WpfApplication1/Ui/Designer/Model/ShapeModel/PushPinShp.cs
+++ SvgDesigner/SvgDesigner/WpfApplication1/Ui/Designer/Model/ShapeModel/PushPinShp.cs
@@ -5,7 +5,5 @@ namespace WpfApplication1.Ui.Designer.Model.ShapeModel
 {
     public class PushPinShp : Shp
     {
-        public double X2 { get; set; } = 10;
-        public double Y2 { get; set; } = -10;
     }
 }
