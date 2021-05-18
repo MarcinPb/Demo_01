b469dbe7abca6e58dfd106948c2233bc8d262bba
Marcin Przybyłowicz <martin.przybylowicz@gmail.com>
2021-05-18 10:06:28 +0200

    MapView is shown on Water Consumption Map tab instead WaterConsumptionReport.EditedView.

--- WbEasyCalc/WbEasyCalc/WpfApplication1/MainWindowViewModel.cs
+++ WbEasyCalc/WbEasyCalc/WpfApplication1/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using GlobalRepository;
 using WpfApplication1.Utility;
 using NLog;
 using WpfApplication1.Ui.Designer.Repo;
+using WpfApplication1.Ui.WaterConsumptionMap;
 
 namespace WpfApplication1
 {
@@ -12,7 +13,8 @@ namespace WpfApplication1
 
 
         public Ui.WbEasyCalcData.ListViewModel WbEasyCalcDataViewModel { get; set; }
-        public Ui.WaterConsumptionReport.EditedViewModel WaterConsumptionReportViewModel { get; set; }
+        //public Ui.WaterConsumptionReport.EditedViewModel WaterConsumptionReportViewModel { get; set; }
+        public MapViewModel WaterConsumptionReportViewModel { get; set; }
 
         public RelayCommand OptionsCmd { get; set; }
         private void OptionsCmdExecute()
@@ -28,7 +30,10 @@ namespace WpfApplication1
             GlobalConfig.InitializeConnection(DatabaseType.Sql);
 
             WbEasyCalcDataViewModel = new Ui.WbEasyCalcData.ListViewModel();
-            WaterConsumptionReportViewModel = new Ui.WaterConsumptionReport.EditedViewModel();
+
+            //WaterConsumptionReportViewModel = new Ui.WaterConsumptionReport.EditedViewModel();
+            WaterConsumptionReportViewModel = new Ui.WaterConsumptionMap.MapViewModel(2021, 5, 6773);
+            WaterConsumptionReportViewModel.WaterConsumptionList = GlobalConfig.DataRepository.WaterConsumptionListRepository.GetList();
 
             // Singleton run before opening designer first time. It takes more or less 5 sek.
             var designerObjList1 = DesignerRepo.DesignerObjList;
